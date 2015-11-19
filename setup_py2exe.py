dcc28a286db9f518ecc6cfee45eed8693a10d422
Gordon Brockway Jr <gordon.brockway@yahoo.com>
2015-11-18 21:29:07 -0500

    Further corrections to script for making an executable. This version creates subfolders in the dist folder for climate data, etc. It creates an exe, but the exe fails to run. Currently, the error has to do with the quit command.

--- setup_py2exe.py
+++ setup_py2exe.py
@@ -1,12 +1,16 @@
+import sys
+
+import glob
+
 from distutils.core import setup
 import py2exe
+import os
 
 # Remove the build folder, a bit slower but ensures that build contains the latest
 import shutil
 shutil.rmtree("build", ignore_errors=True)
 
-# my setup.py is based on one generated with gui2exe, so data_files is done a bit differently
-data_files = []
+matplot_lib_data_files = []
 includes = []
 excludes = ['_gtkagg', '_tkagg', 'bsddb', 'curses', 'pywin.debugger',
             'pywin.debugger.dbgcon', 'pywin.dialogs', 'tcl',
@@ -21,23 +25,15 @@ other_resources = []
 
 # add the mpl mpl-data folder and rc file
 import matplotlib as mpl
-data_files += mpl.get_py2exe_datafiles()
-data_files += "./Cold Climate Air-Source Heat Pump Listing.txt"
-#data_files += "Climate Data/KBED-2002.txt"
-# data_files += "Climate Data/KBED-2003.txt"
-# data_files += "Climate Data/KBED-2004.txt"
-# data_files += "Climate Data/KBED-2005.txt"
-# data_files += "Climate Data/KBED-2006.txt"
-# data_files += "Climate Data/KBED-2007.txt"
-# data_files += "Climate Data/KBED-2008.txt"
-# data_files += "Climate Data/KBED-2009.txt"
-# data_files += "Climate Data/KBED-2010.txt"
-# data_files += "Climate Data/KBED-2011.txt"
-# data_files += "Climate Data/KBED-2012.txt"
-# data_files += "Climate Data/KBED-2013.txt"
-# data_files += "Climate Data/KBED-2014.txt"
-# data_files += "Climate Data/KBED-2015.txt"
-# data_files += "Residential Profiles/Default oil deliveries.txt"
+matplot_lib_data_files += mpl.get_py2exe_datafiles()
+
+
+f2 = ("", [r'./Cold Climate Air-Source Heat Pump Listing.txt'])
+matplot_lib_data_files.append(f2)
+
+matplot_lib_data_files.append(("Climate Data", glob.glob(r'.\Climate Data\*.*')))
+
+matplot_lib_data_files.append(("Residential Profiles", glob.glob(r'.\Residential Profiles\Default oil deliveries.txt')))
 
 setup(
     windows=[{"script" : "Heat Pump Analysis.py"}],
@@ -62,5 +58,5 @@ setup(
     # using zipfile to reduce number of files in dist
     zipfile = r'lib\library.zip',
 
-    data_files=data_files
+    data_files=matplot_lib_data_files
 )
